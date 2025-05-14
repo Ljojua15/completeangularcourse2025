59a0e371d59de34b7ed468edffd6d8f213865b9e
ljojua <ljojua@crocobet.com>
2025-05-14 10:27:48 +0400

    add home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ReservationListComponent } from './components/reservation-list/reservation-list.component';
+import { ReservationFormComponent } from './components/reservation-form/reservation-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", component:HomeComponent},
+    {path:"list", component: ReservationListComponent},
+    {path:"new", component:ReservationFormComponent},
+    {path:"**", component:HomeComponent},
+
+];
