17fca20aa0cba254b485463e5385c5d21e8a8fec
ljojua <ljojua@crocobet.com>
2025-05-14 13:00:20 +0400

    feat: add forms

--- src/app/components/reservation-form/reservation-form.component.ts
+++ src/app/components/reservation-form/reservation-form.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import {FormsModule, FormGroup,  ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule,],
   templateUrl: './reservation-form.component.html',
   styleUrl: './reservation-form.component.scss',
   standalone:true
 })
 export class ReservationFormComponent {
+ public reservationForm: FormGroup = new FormGroup({});
 
+
+
+ public onSubmit(){
+
+ }
 }
