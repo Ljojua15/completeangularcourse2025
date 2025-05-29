545323bdc10a4f253928fd3f388f582d905c08ca
ljojua <ljojua@crocobet.com>
2025-05-29 16:09:22 +0400

    all

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,13 +18,22 @@ export const routes: Routes = [
     // {path:"**", component:HomeComponent},
 
     
-    {path:"", component:MainContentComponent},
-    {path:"posts", component: PostsComponent},
-    {path:"comments", component:CommentsComponent},
-    {path:"albums", component:AlbumsComponent},
-    {path:"photos", component: PhotosComponent},
-    {path:"todos", component:TodosComponent},
-    {path:"users", component:UsersComponent},
-    {path:"**", component:MainContentComponent},
+    // {path:"", component:MainContentComponent},
+    // {path:"posts", component: PostsComponent},
+    // {path:"comments", component:CommentsComponent},
+    // {path:"albums", component:AlbumsComponent},
+    // {path:"photos", component: PhotosComponent},
+    // {path:"todos", component:TodosComponent},
+    // {path:"users", component:UsersComponent},
+    // {path:"**", component:MainContentComponent},
+
+  { path: "", loadComponent: () => import('./components/json/main-content/main-content.component').then(m => m.MainContentComponent) },
+  { path: "posts", loadComponent: () => import('./components/json/posts/posts.component').then(m => m.PostsComponent) },
+  { path: "comments", loadComponent: () => import('./components/json/comments/comments.component').then(m => m.CommentsComponent) },
+  { path: "albums", loadComponent: () => import('./components/json/albums/albums.component').then(m => m.AlbumsComponent) },
+  { path: "photos", loadComponent: () => import('./components/json/photos/photos.component').then(m => m.PhotosComponent) },
+  { path: "todos", loadComponent: () => import('./components/json/todos/todos.component').then(m => m.TodosComponent) },
+  { path: "users", loadComponent: () => import('./components/json/users/users.component').then(m => m.UsersComponent) },
+  { path: "**", redirectTo: "" },
 
 ];
--- src/app/lib/services/posts.service.ts
+++ src/app/lib/services/posts.service.ts
@@ -1,10 +1,17 @@
-import { Injectable } from '@angular/core';
-
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IUser } from '../interface/users.interface';
+import {HttpClient} from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
-  private postApi = 'https://jsonplaceholder.typicode.com'
+  private postApi = 'https://jsonplaceholder.typicode.com';
+    private http = inject(HttpClient)
 
-  
+  public getUsers$() : Observable<IUser[]> {
+    return this.http.get<IUser[]>(this.postApi + "/users")
+  }
+
+
 }
