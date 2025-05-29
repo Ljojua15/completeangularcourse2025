1a645ec4cba21fa6d7cb72a4d28512124db32dc5
ljojua <ljojua@crocobet.com>
2025-05-29 16:09:09 +0400

    test

--- src/app/components/json/users/users.component.ts
+++ src/app/components/json/users/users.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { IUser } from '../../../lib/interface/users.interface';
+import { PostsService } from '../../../lib/services/posts.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
 export class UsersComponent {
+
+private readonly postsService = inject(PostsService)
+public users : IUser[] = []
+
+public usersList$ = this.postsService.getUsers$()
 
 }
